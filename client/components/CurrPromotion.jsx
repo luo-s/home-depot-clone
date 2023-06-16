b1604c050e768f051a4008577d30a396c04300d9
Shu <luo.s@outlook.com>
2023-06-15 17:37:32 -0800

    add CurrPromotion

--- client/components/CurrPromotion.jsx
+++ client/components/CurrPromotion.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { FaChevronRight } from "react-icons/fa";
 
 const CurrPromotion = () => {
-  return;
+  return (
+    <div className="CurrPromotion">
+      FATHER'S DAY IS JUNE 18TH. Gifts for Dad. Shop Now <FaChevronRight />
+    </div>
+  );
 };
 
 export default CurrPromotion;

e8d60cbebca255dad1a66ff76782afe0cb2d7a4f
Shu <luo.s@outlook.com>
2023-06-15 18:22:04 -0800

    in progress

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,7 +1,32 @@
 import React, { useEffect, useState } from "react";
 
 const Carousel = () => {
-  return;
+  return (
+    <main>
+      <div>This is a test</div>
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="..." className="d-block w-100" alt="...">
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="...">
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </main>
+  );
 };
 
 export default Carousel;
